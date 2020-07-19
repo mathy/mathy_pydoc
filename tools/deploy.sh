be7be55bd3eab4dad6cea34d8e333f58bec21d62
justindujardin <justin@dujardinconsulting.com>
2020-07-18 18:17:09 -0700

    fix(ci): do version increment during deploy step

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-
+echo "Installing semantic-release requirements"
+npm install 
+echo "Updating build version"
+npx ts-node tools/set-build-version.ts
 echo "Running semantic-release"
 npx semantic-release
 
