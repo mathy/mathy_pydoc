8ff1f8463eafe6d1effe5e4a06507678e15b4627
Justin DuJardin <justin@dujardinconsulting.com>
2022-05-16 16:33:13 -0700

    fix: deploy updated package on merge
     - semantic release commit doesn't trigger a github action run

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,6 +5,13 @@ set -e
 . .env/bin/activate
 git config --global user.email "justin@dujardinconsulting.com"
 git config --global user.name "justindujardin"
+
+
+echo "Installing semantic-release requirements"
+npm install 
+echo "Updating build version"
+npx ts-node tools/set-build-version.ts
+
 echo "Build and publish to pypi..."
 rm -rf build dist
 echo "--- Install requirements"
@@ -20,15 +27,5 @@ set +e
 twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} dist/*
 rm -rf build dist
 
-
-# The build/deploy step runs in two commits. The first runs the tests and generates a
-# tag commit, and that tag commit deploys to pypi. To make this work we do the pypi
-# bit first, then run semantic-release to bump versions and generate a new commit. This
-# way the package is always published from the tag commit, and not accidentally by the
-# initiating change because the version was already bumped.
-echo "Installing semantic-release requirements"
-npm install 
-echo "Updating build version"
-npx ts-node tools/set-build-version.ts
 echo "Running semantic-release"
 npx semantic-release
